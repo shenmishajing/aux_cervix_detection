3837fd808be11c5149bb2b4acaf45b3fbfe3b463
shenmishajing <shenmishajing@gmail.com>
2022-01-31 09:01:31 +0800

    change package name to default-project

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'dual_cervix_registration',  # 包名字
+    name = 'default-project',  # 包名字
     version = '1.0',  # 包版本
     packages = find_packages(),  # 包
 )
