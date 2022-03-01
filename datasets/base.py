d84ef5dbd4ea879511e2c775c9f2b7f6264bdebb
shenmishajing <shenmishajing@gmail.com>
2022-03-01 11:12:55 +0800

    add dataloader_config per split feature in LightningDataModule

--- datasets/base.py
+++ datasets/base.py
@@ -1,53 +1,70 @@
+import copy
 from collections.abc import Mapping, Sequence
-from typing import Callable, Optional
 
 from pytorch_lightning.core.datamodule import LightningDataModule as _LightningDataModule
 from torch.utils.data import DataLoader, IterableDataset
+
+from utils.cli.yaml import deep_update
 
 
 class LightningDataModule(_LightningDataModule):
     def __init__(self, data_loader_config = None, split_name_map = None):
         super().__init__()
-        for name in ['train', 'val', 'test', 'predict']:
-            if split_name_map is not None:
-                if name in split_name_map:
-                    setattr(self, name + '_name', split_name_map[name])
-                    continue
-            if name == 'predict':
-                setattr(self, name + '_name', 'test')
-            else:
-                setattr(self, name + '_name', name)
-        if data_loader_config is None:
-            self.data_loader_config = {}
+        split_names = ['train', 'val', 'test', 'predict']
+        if split_name_map is None:
+            self.split_name_map = {split_name: split_name for split_name in split_names}
         else:
-            self.data_loader_config = data_loader_config
+            self.split_name_map = split_name_map
+        for name in split_names:
+            self.split_name_map.setdefault(name, name if name != 'predict' else 'test')
 
-        self.train_dataset = self.val_dataset = self.test_dataset = self.predict_dataset = None
+        self.data_loader_config = {} if data_loader_config is None else data_loader_config
+
+        if all([self.data_loader_config.get(name) is None for name in split_names]):
+            self.data_loader_config = {name: copy.deepcopy(self.data_loader_config) for name in split_names}
+        else:
+            for main_name in split_names:
+                if self.data_loader_config.get(main_name) is not None:
+                    for name in [n for n in split_names if n != main_name]:
+                        if self.data_loader_config.get(name) is None:
+                            self.data_loader_config[name] = copy.deepcopy(self.data_loader_config[main_name])
+                        else:
+                            self.data_loader_config[name] = deep_update(copy.deepcopy(self.data_loader_config[main_name]),
+                                                                        self.data_loader_config[name])
+                    break
+
+        self.datasets = {}
+
+    def _setup_dataset(self, split_name):
+        self.datasets[split_name] = self._build_data_set(self.split_name_map[split_name])
 
     def setup(self, stage = None):
         if self.trainer.overfit_batches > 0:
-            self.train_dataset = self._build_data_set(self.train_name)
-            return
-        if stage in [None, 'fit']:
-            self.train_dataset = self._build_data_set(self.train_name)
-        if stage in [None, 'fit', 'validate']:
-            self.val_dataset = self._build_data_set(self.val_name)
-        if stage in [None, 'test']:
-            self.test_dataset = self._build_data_set(self.test_name)
-        if stage in [None, 'predict']:
-            self.predict_dataset = self._build_data_set(self.predict_name)
+            split_names = ['train']
+        elif stage is None:
+            split_names = ['train', 'val', 'test', 'predict']
+        elif stage == 'fit':
+            split_names = ['train', 'val']
+        else:
+            split_names = [stage]
+
+        for name in split_names:
+            self._setup_dataset(name)
+
+    def _dataloader(self, split_name, **kwargs):
+        return self._build_data_loader(self.datasets[split_name], split = split_name, **kwargs)
 
     def train_dataloader(self):
-        return self._build_data_loader(self.train_dataset, shuffle = True)
+        return self._dataloader('train', shuffle = True)
 
     def val_dataloader(self):
-        return self._build_data_loader(self.val_dataset)
+        return self._dataloader('val')
 
     def test_dataloader(self):
-        return self._build_data_loader(self.test_dataset)
+        return self._dataloader('test')
 
     def predict_dataloader(self):
-        return self._build_data_loader(self.predict_dataset)
+        return self._dataloader('predict')
 
     def _build_data_set(self, split):
         raise NotImplementedError
@@ -56,16 +73,19 @@ class LightningDataModule(_LightningDataModule):
     def collate(batch):
         raise NotImplementedError
 
-    def _build_data_loader(self, dataset, shuffle: Optional[bool] = False, collate_fn: Optional[Callable] = None):
+    def _build_data_loader(self, dataset, shuffle = False, collate_fn = None, split = 'train'):
         def dataloader(ds, cl_fn) -> DataLoader:
-            return DataLoader(ds, shuffle = shuffle and not isinstance(ds, IterableDataset), collate_fn = cl_fn, **self.data_loader_config)
+            return DataLoader(ds, shuffle = shuffle and not isinstance(ds, IterableDataset), collate_fn = cl_fn,
+                              **self.data_loader_config[split])
 
         if collate_fn is None:
             collate_fn = self.collate
         if isinstance(dataset, Mapping):
-            return {key: dataloader(ds, cl_fn = collate_fn[key] if isinstance(collate_fn, Mapping) else collate_fn) for key, ds in
-                    dataset.items()}
+            return {key: self._build_data_loader(ds, shuffle = shuffle,
+                                                 collate_fn = collate_fn[key] if isinstance(collate_fn, Mapping) else collate_fn,
+                                                 split = split) for key, ds in dataset.items()}
         if isinstance(dataset, Sequence):
-            return [dataloader(dataset[i], cl_fn = collate_fn[i] if isinstance(collate_fn, Sequence) else collate_fn) for i in
-                    range(len(dataset))]
+            return [self._build_data_loader(dataset[i], shuffle = shuffle,
+                                            collate_fn = collate_fn[i] if isinstance(collate_fn, Sequence) else collate_fn, split = split)
+                    for i in range(len(dataset))]
         return dataloader(dataset, cl_fn = collate_fn)
