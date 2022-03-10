03c42971e4824b37d15bbfd16dca50ab895f66ba
shenmishajing <shenmishajing@gmail.com>
2022-03-10 22:05:30 +0800

    fix do not import KFoldMMDetDataSetAdapter in datasets/mmdatasets bug

--- datasets/mmdatasets/__init__.py
+++ datasets/mmdatasets/__init__.py
@@ -1,6 +1,6 @@
 from .cataract import CataractDataSet
 from .dual_cervix import DualCervixDataSet
-from .mmdataset_adapter import MMDetDataSetAdapter
+from .mmdataset_adapter import KFoldMMDetDataSetAdapter, MMDetDataSetAdapter
 from .multi_modals import MultiModalsDataSet
 from .tct import TCTDataSet
 from .transforms import *
