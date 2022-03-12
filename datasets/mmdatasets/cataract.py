e2e10c52bd0a38a9bae13c77d955063be63c4c4c
shenmishajing <shenmishajing@gmail.com>
2022-03-12 18:40:15 +0800

    add check and add_2 two version of cataract_classification dataset

--- datasets/mmdatasets/cataract.py
+++ datasets/mmdatasets/cataract.py
@@ -9,12 +9,17 @@ from mmdet.datasets import DATASETS
 class CataractDataSet(BaseDataset):
     CLASSES = [str(i) for i in range(7)]
 
+    def __init__(self, labels = None, *args, **kwargs):
+        if labels is None:
+            labels = ['gt_label']
+        self.labels = labels
+        super().__init__(*args, **kwargs)
+
     def load_annotations(self):
         data_infos = []
         for line in csv.reader(open(self.ann_file)):
             info = {'img_prefix': self.data_prefix, 'img_info': {'filename': line[0]}}
-            for i, name in enumerate(['gt_label', 'center_label', 'center_turbidity', 'center_pervade_turbidity',
-                                      'border_label', 'border_turbidity', 'border_pervade_turbidity']):
+            for i, name in enumerate(self.labels):
                 info[name] = np.array(int(line[i + 1]), dtype = np.long)
             data_infos.append(info)
         return data_infos
