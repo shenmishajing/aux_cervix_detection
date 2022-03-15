2f43fd0c88b8dd924bf9507495c0eb5bd6bb2ae9
shenmishajing <shenmishajing@gmail.com>
2022-03-15 15:10:03 +0800

    rename trainer/global_step to global_step in wandb logger

--- utils/loggers/wandb.py
+++ utils/loggers/wandb.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Dict, Optional
 
 from pytorch_lightning.loggers.wandb import WandbLogger
+from pytorch_lightning.utilities import rank_zero_only
 
 
 class WandbNamedLogger(WandbLogger):
@@ -14,3 +15,13 @@ class WandbNamedLogger(WandbLogger):
         """
         # don't create an experiment if we don't have one
         return self._experiment.name if self._experiment else self._name
+
+    @rank_zero_only
+    def log_metrics(self, metrics: Dict[str, float], step: Optional[int] = None) -> None:
+        assert rank_zero_only.rank == 0, "experiment tried to log from global_rank != 0"
+
+        metrics = self._add_prefix(metrics)
+        if step is not None:
+            self.experiment.log({**metrics, "global_step": step})
+        else:
+            self.experiment.log(metrics)
