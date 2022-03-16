4472cfbbf73c4a81151daa18d7df0570e75a6e21
shenmishajing <shenmishajing@gmail.com>
2022-03-16 09:53:55 +0800

    add mse loss and smooth l1 loss

--- models/models/mmclassifiers/__init__.py
+++ models/models/mmclassifiers/__init__.py
@@ -3,4 +3,5 @@ from .image import ImageClassifier
 from .image_rotate import RotateImageClassifier
 from .image_with_label import ImageWithLabelClassifier
 from .label import LabelClassifier
+from .losses import *
 from .mmcls_adapter import MMClsModelAdapter
---
+++ models/models/mmclassifiers/losses/__init__.py
@@ -0,0 +1,2 @@
+from .mse import MSELoss
+from .smooth_l1 import SmoothL1Loss
