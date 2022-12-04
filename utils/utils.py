79ee9c0581095a132dd9055eae650b3ea9f38e69
shenmishajing <shenmishajing@gmail.com>
2022-12-04 19:00:51 +0800

    move model checkpoint callback to the last callback use checkpoint_callback.__resolve_ckpt_dir to get log dir

--- utils/utils.py
+++ utils/utils.py
@@ -3,12 +3,14 @@ import sys
 
 
 def get_log_dir(trainer):
-    if trainer.checkpoint_callback is not None and trainer.checkpoint_callback.dirpath is not None:
-        log_dir = os.path.dirname(trainer.checkpoint_callback.dirpath)
+    if trainer.checkpoint_callback is not None:
+        if trainer.checkpoint_callback.dirpath is not None:
+            dir_path = trainer.checkpoint_callback.dirpath
+        else:
+            dir_path = trainer.checkpoint_callback.__resolve_ckpt_dir(trainer)
+        log_dir = os.path.dirname(dir_path)
     else:
-        save_dir = trainer.default_root_dir
-        version = trainer.logger.version if isinstance(trainer.logger.version, str) else f"version_{trainer.logger.version}"
-        log_dir = os.path.join(save_dir, str(trainer.logger.name), version)
+        log_dir = trainer.default_root_dir
     return log_dir
 
 
