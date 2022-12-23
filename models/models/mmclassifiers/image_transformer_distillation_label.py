3597975f04e820f3396fa38360576a1a48877228
shenmishajing <shenmishajing@gmail.com>
2022-12-23 17:22:48 +0800

    add stack head and label distillation feature

--- models/models/mmclassifiers/image_transformer_distillation_label.py
+++ models/models/mmclassifiers/image_transformer_distillation_label.py
@@ -16,7 +16,11 @@ class ImageTransformerDistillationLabelTeacherClassifier(
             x = x[-1]
 
         tokens = self.token_forward(x, label)
-        return self.extract_all_cls_tokens(tokens)
+        cls_token = self.extract_all_cls_tokens(tokens)
+        return {
+            "cls_token": cls_token,
+            "label": self.head.pre_logits(cls_token[-1]),
+        }
 
 
 class ImageTransformerDistillationLabelClassifier(ImageTransformerClassifier):
@@ -25,7 +29,7 @@ class ImageTransformerDistillationLabelClassifier(ImageTransformerClassifier):
         teacher_classifier: nn.Module = None,
         distillation_loss=None,
         *args,
-        **kwargs
+        **kwargs,
     ):
         super().__init__(*args, **kwargs)
         self.teacher_classifier = teacher_classifier
@@ -34,10 +38,14 @@ class ImageTransformerDistillationLabelClassifier(ImageTransformerClassifier):
             for p in self.teacher_classifier.parameters():
                 p.requires_grad = False
 
-        if distillation_loss is not None:
-            self.distillation_loss = build_loss(distillation_loss)
-        else:
-            self.distillation_loss = None
+        if distillation_loss is None:
+            distillation_loss = {}
+        self.distillation_loss = nn.ModuleDict()
+        for k, v in distillation_loss.items():
+            if isinstance(v, dict):
+                self.distillation_loss[k] = build_loss(v)
+            elif isinstance(v, nn.Module):
+                self.distillation_loss[k] = v
 
     def forward_train(self, img, label, gt_label, **kwargs):
         """Forward computation during training.
@@ -61,22 +69,25 @@ class ImageTransformerDistillationLabelClassifier(ImageTransformerClassifier):
 
         tokens = self.token_forward(x, label.to(x.dtype))
         cls_token = self.extract_all_cls_tokens(tokens)
-        distillation_cls_token = self.teacher_classifier.distillation_forward(
+        distillation_target = self.teacher_classifier.distillation_forward(
             img, label.to(x.dtype)
         )
+
+        distillation_predict = {
+            "cls_token": cls_token,
+            "label": self.head.pre_logits(cls_token[-1]),
+        }
 
         losses = dict()
         losses["loss_cls"] = self.head.forward_train(cls_token[-1], gt_label, **kwargs)[
             "loss"
         ]
-        if self.distillation_loss is not None:
-            losses["loss_distillation"] = torch.mean(
-                torch.stack(
-                    [
-                        self.distillation_loss(t, d)
-                        for t, d in zip(cls_token, distillation_cls_token)
-                    ]
-                )
+        for name, loss in self.distillation_loss.items():
+            target, predict = distillation_target[name], distillation_predict[name]
+            if not isinstance(target, list):
+                target, predict = [target], [predict]
+            losses[f"loss_{name}_distillation"] = torch.mean(
+                torch.stack([loss(t, p) for t, p in zip(target, predict)])
             )
         if self.train_teacher:
             losses["loss_teacher"] = self.teacher_classifier.forward_train(
